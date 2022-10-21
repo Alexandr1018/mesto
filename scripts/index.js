af6f8b89c6626e449e1a7491321ce9a32ec27ad0
Alexander Makarov <makarov.alexander.work@yandex.ru>
2022-10-21 21:36:44 +0300

    fix: заменить теги в html, изменить функцию редактирования профиля в JS, добавить выравнивание попапа по центру экрана в CSS

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,37 @@
-// Делаем попап редактирования профиля: открытие попапа, закрытие попапа
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__edit-button");
+const profileElement = document.querySelector(".profile");
+const formElement = popupElement.querySelector(".popup__form");
+const popupTextName = formElement.querySelector(".popup__text_name");
+const popupTextJob = formElement.querySelector(".popup__text_job");
+let profileName = profileElement.querySelector(".profile__profile-text_name");
+let profileJob = profileElement.querySelector(".profile__profile-text_job");
 
+// Делаем попап редактирования профиля: открытие попапа, закрытие попапа
 const togglePopupVisibility = function () {
   popupElement.classList.toggle("popup_opened");
+  // заменяем значения в форме, на те, что отображабтся на странице
+  if (popupElement.classList.contains("popup_opened")) {
+    popupTextName.value = profileName.textContent;
+    popupTextJob.value = profileJob.textContent;
+  }
 };
 
 popupOpenButtonElement.addEventListener("click", togglePopupVisibility);
 popupCloseButtonElement.addEventListener("click", togglePopupVisibility);
 
-// Делаем попап редактирования профиля: подтверждение нового профиля
-const profileElement = document.querySelector(".profile");
-const formElement = popupElement.querySelector(".popup__container");
-const popupTextName = formElement.querySelector("#popup-text-name");
-const popupTextJob = formElement.querySelector("#popup-text-job");
-let profileName = profileElement.querySelector("#profile-name");
-let profileJob = profileElement.querySelector("#profile-job");
 
+
+// Делаем попап редактирования профиля: подтверждение нового профиля
 function formSubmitHandler(event) {
   // отменяем стандартное поведение кнопки при нажатии
   event.preventDefault();
   // заменяем значения полей в секции profile на введенные в форме
   profileName.textContent = popupTextName.value;
   profileJob.textContent = popupTextJob.value;
-  // заменяем значения в форме, на те, что отображабтся на странице
-  popupTextName.setAttribute("value", profileName.textContent);
-  popupTextJob.setAttribute("value", profileJob.textContent);
   // закрываем попап
-  popupElement.classList.toggle("popup_opened");
+  togglePopupVisibility();
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
