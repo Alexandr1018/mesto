8712805986aa33e3d11578cca725682bacf0e11c
Alexander Makarov <makarov.alexander.work@yandex.ru>
2022-11-03 00:28:16 +0300

    fix: изменить названия функция, сделать отдельные функции на открытие и закрытие попапов, создать дополнительный js файл с данными, переделать обнуление форм, корректировка стиля попапа

--- scripts/index.js
+++ scripts/index.js
@@ -26,14 +26,14 @@ const popupTextUrl = formElementAddNewCard.querySelector(".popup__text_input_url
 
 //Переменные попапа увеличения картинки
 //переменная хранит попап увеличения картинки
-const popupPhotoReveal = document.querySelector(".popup_photo-reveal");
+const popupPhoto = document.querySelector(".popup_photo");
 //кнопка закрытия попапа увеличения картинки
-const popupClosePhoroRevealButton = popupPhotoReveal.querySelector(".popup__close");
+const popupClosePhoroRevealButton = popupPhoto.querySelector(".popup__close");
 //переменная открытия попапа увеличения картинки
 // const elementTopSide = document.querySelector(".elements__top-side");
 //переменные попапа увеличения картинки
-const popupPhotoRevealImageBig = popupPhotoReveal.querySelector(".popup__image-big");
-const popupPhotoRevealImageCaption = popupPhotoReveal.querySelector(".popup__image-caption");
+const popupPhotoRevealImageBig = popupPhoto.querySelector(".popup__image-big");
+const popupPhotoRevealImageCaption = popupPhoto.querySelector(".popup__image-caption");
 
 
 //переменные секции профиля
@@ -43,144 +43,104 @@ const profileJob = profileElement.querySelector(".profile__profile-job");
 
 //переменные для хранения шаблона добавления новых карточек
 const templateElement = document.querySelector("#template-element").content;
-const elementsList = document.querySelector(".elements__list");
+const elementsContainer = document.querySelector(".elements__list");
 
-//переменная в которой хранятся предзагруженные карточки
-const preloadedCards = [
-  {
-    name: "Байкал",
-    link: "https://images.unsplash.com/photo-1551845041-63e8e76836ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80"
-  },
-  {
-    name: "Иваново",
-    link: "https://images.unsplash.com/photo-1527440050868-25ac612aae1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-  },
-  {
-    name: "Камчатка",
-    link: "https://images.unsplash.com/photo-1557094005-176cbfe3554d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1634&q=80"
-  },
-  {
-    name: "Красноярск",
-    link: "https://images.unsplash.com/photo-1587451152235-05466c2fc532?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-  },
-  {
-    name: "Москва",
-    link: "https://images.unsplash.com/photo-1512495039889-52a3b799c9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-  },
-  {
-    name: "Тюмень",
-    link: "https://images.unsplash.com/photo-1621878983992-bac95a1e8dd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
-  }
-];
+
+//функция открытия всех попапов
+const openPopup = popup => popup.classList.add("popup_opened");
+//функция закрытия всех попапов
+const closePopup = popup => popup.classList.remove("popup_opened");
+
+// функция создания новой карточки
+const createNewCard = (link, name) => {
+  const userElement = templateElement.querySelector(".template-element").cloneNode(true);
+  userElement.querySelector(".elements__top-side").src = link;
+  userElement.querySelector(".elements__caption").textContent = name;
+  userElement.querySelector(".elements__top-side").alt = `Фото - ${name}`;
+  return userElement;
+}
+
+// функция добавления созданной новой карточки
+const addNewCard = (link, name) => {
+  createNewCard(link, name);
+  elementsContainer.prepend(createNewCard(link, name));
+  elementsContainer.querySelector(".elements__button-like").addEventListener("click", likeCard);
+  elementsContainer.querySelector(".elements__button-delete").addEventListener("click", deleteCard);
+  elementsContainer.querySelector(".elements__top-side").addEventListener("click", openCard);
+}
 
 // ПОПАП РЕДАКТИРОВАНИЯ ПРОФИЛЯ
 // Делаем попап редактирования профиля: открытие попапа, закрытие попапа
-const togglePopupProfileChangerVisibility =  () => {
-  popupProfileChanger.classList.toggle("popup_opened");
+const openPopupProfileChanger =  () => {
+  openPopup(popupProfileChanger);
   // заменяем значения в форме, на те, что отображабтся на странице
-  if (popupProfileChanger.classList.contains("popup_opened")) {
-    popupTextName.value = profileName.textContent;
-    popupTextJob.value = profileJob.textContent;
-  }
+  popupTextName.value = profileName.textContent;
+  popupTextJob.value = profileJob.textContent;
 };
 //Слушатели на открытие и закрытие попапа редактирования профиля
-popupOpenButtonElement.addEventListener("click", togglePopupProfileChangerVisibility);
-popupCloseProfileButton.addEventListener("click", togglePopupProfileChangerVisibility);
+popupOpenButtonElement.addEventListener("click", openPopupProfileChanger);
+popupCloseProfileButton.addEventListener("click", () => closePopup(popupProfileChanger));
 // Делаем попап редактирования профиля: подтверждение нового профиля
-function formSubmitProfileChanger(event) {
+function submitFormProfileChanger(evt) {
   // отменяем стандартное поведение кнопки при нажатии
-  event.preventDefault();
+  evt.preventDefault();
   // заменяем значения полей в секции profile на введенные в форме
   profileName.textContent = popupTextName.value;
   profileJob.textContent = popupTextJob.value;
   // закрываем попап
-  togglePopupProfileChangerVisibility();
+  closePopup(popupProfileChanger);
 };
-formElementProfileChanger.addEventListener("submit", formSubmitProfileChanger);
+formElementProfileChanger.addEventListener("submit", submitFormProfileChanger);
 
 //ДОБАВЛЕНИЕ КАРТОЧКИ
 //делаем функцию предзагрузки карточек с картинками
 preloadedCards.forEach( item => {
-  const userElement = templateElement.querySelector(".template-element").cloneNode(true);
-  userElement.querySelector(".elements__top-side").src = item.link;
-  userElement.querySelector(".elements__caption").textContent = item.name;
-  elementsList.prepend(userElement);
-  //лайк предзагруженных карточек
-  likeCard();
-  //удаление предзагруженных карточек
-  deleteCard();
+  addNewCard(item.link, item.name);
   //увеличение предзагруженных картинок
-  openCard();
+  // openCard();
 });
 
 // ПОПАП ДОБАВЛЕНИЯ НОВОЙ КАРТОЧКИ
 // Делаем попап добавления новой карточки: открытие попапа, закрытие попапа
-const togglePopupAddNewCardVisibility = () => {
-  popupAddNewCard.classList.toggle("popup_opened");
-};
-popupAddNewCardOpenButton.addEventListener("click", togglePopupAddNewCardVisibility);
-popupAddNewCardCloseButton.addEventListener("click", togglePopupAddNewCardVisibility);
-//функция добавления новой карточки
-function addNewCard() {
-  const userElement = templateElement.querySelector(".template-element").cloneNode(true);
-  userElement.querySelector(".elements__top-side").src = popupTextUrl.value;
-  userElement.querySelector(".elements__caption").textContent = popupTextLocationName.value;
-  elementsList.prepend(userElement);
-};
+popupAddNewCardOpenButton.addEventListener("click", () => openPopup(popupAddNewCard));
+popupAddNewCardCloseButton.addEventListener("click", () => closePopup(popupAddNewCard));
 //функция подтверждения добавления новой карточки
-function formSubmitAddNewCard(event) {
+function submitFormAddNewCard(event) {
   // отменяем стандартное поведение кнопки при нажатии
   event.preventDefault();
   // добавляем новую карточку
-  addNewCard();
+  addNewCard(popupTextUrl.value, popupTextLocationName.value);
   // закрываем попап
-  togglePopupAddNewCardVisibility();
+  closePopup(popupAddNewCard);
   // обнуляем поля в форме
-  popupTextUrl.value = "";
-  popupTextLocationName.value = "";
-  //лайк новых карточек
-  likeCard();
-  //удаление новых карточек
-  deleteCard();
+  formElementAddNewCard.reset();
   //увеличение предзагруженных картинок
-  openCard();
+  // openCard();
 };
-formElementAddNewCard.addEventListener("submit", formSubmitAddNewCard);
+formElementAddNewCard.addEventListener("submit", submitFormAddNewCard);
 
 //ЛАЙК КАРТОЧКИ
-function likeCard() {
-  document.querySelector(".elements__button-like").addEventListener("click", function(evt) {
-    evt.target.classList.toggle("elements__button-like_active");
-  });
+function likeCard(evt) {
+  evt.target.classList.toggle("elements__button-like_active");
 };
 
 //УДАЛЕНИЕ КАРТОЧКИ
-function deleteCard() {
-  document.querySelector(".elements__button-delete").addEventListener("click", function(evt) {
-    evt.target.closest(".template-element").remove();
-  })
+function deleteCard(evt) {
+  evt.target.closest(".template-element").remove();
 };
 
 //ПОПАП УВЕЛИЧЕНИЯ КАРТОЧКИ
 // Делаем попап увеличения картинки: открытие попапа, закрытие попапа
-function togglePopupPhotoRevealVisability() {
-  popupPhotoReveal.classList.toggle("popup_opened");
-  if (!popupPhotoReveal.classList.contains("popup_opened")) {
-    popupPhotoRevealImageBig.src = "";
-    popupPhotoRevealImageCaption.textContent = "";
-  }
+function closePopupPhotoRevealVisability() {
+  closePopup(popupPhoto);
+  popupPhotoRevealImageBig.src = "";
+  popupPhotoRevealImageCaption.textContent = "";
 };
 //Слушатель на  открытие и закрытие попапа увеличения картинки
-function openCard() {
-  const elementTopSide = document.querySelector(".elements__top-side");
-  elementTopSide.addEventListener("click", function(evt) {
-    popupPhotoRevealImageBig.src = evt.target.src;
-    popupPhotoRevealImageCaption.textContent = evt.target.nextElementSibling.textContent;
-    // console.log(popupPhotoRevealImageBig.src);
-    togglePopupPhotoRevealVisability();
-    // popupPhotoRevealImageBig.src = "";
-    // console.log(popupPhotoRevealImageBig.src);
-  });
-  popupClosePhoroRevealButton.addEventListener("click", togglePopupPhotoRevealVisability);
-
+function openCard(evt) {
+  popupPhotoRevealImageBig.src = evt.target.src;
+  popupPhotoRevealImageCaption.textContent = evt.target.alt.split(" ").pop();
+  popupPhotoRevealImageBig.alt = `Фото в увеличенном виде - ${evt.target.alt.split(" ").pop()}`;
+  openPopup(popupPhoto);
+  popupClosePhoroRevealButton.addEventListener("click", closePopupPhotoRevealVisability);
 }
